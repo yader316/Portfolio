79532d4bbf65357a1cf91608ee560f89c7d87a66
yader mendoza <yadtais316@gmail.com>
2020-02-03 22:06:52 -0600

    se termino en la parte de about me, referencia, projec, estudios se agrego tambien el scroll top, al momento que este col-xs se ponen la barra porcentaje

--- dev/js/progressBar.js
+++ dev/js/progressBar.js
@@ -1,5 +1,10 @@
-$('.progressbar__round').circleProgress({fill: {color:'#0fbf61'}})
-.on('circle-animation-progress', function(event, progress, stepValue){
-
-    $(this).find('.progressbar__circleContainer').text(String(stepValue.toFixed(2)).substr(2) + '%');
-});
+// btn click para refrescar y ver mas   
+function Circlle(el) {
+    $(el).circleProgress({fill: {color:'#0fbf61'}})
+    .on('circle-animation-progress', function(event, progress, stepValue){
+  
+        $(this).find('.progressbar__circleContainer').text(String(stepValue.toFixed(2)).substr(2) + '%');
+    });
+  }
+  
+  Circlle('.progressbar__round');
